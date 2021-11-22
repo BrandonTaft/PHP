d70a62aaadce797cf8cd7a61bed087cb542be562
Btaft6278 <btaft6278@gmail.com>
2021-11-21 21:38:38 -0500

    initial commit

---
+++ twilio-php-app/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once "vendor/autoload.php";
+
+use Twilio\TwiML\VoiceResponse;
+
+$response = new VoiceResponse;
+$response->say("Hello World!");
+
+header("content-type: text/xml");
+echo $response;
---
+++ twilio-php-app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'brandontaft/twilio-php-app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brandontaft/twilio-php-app' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '6.31.1',
+            'version' => '6.31.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'reference' => 'b3788dd0171bc8037206c6760174315bbe9c3443',
+            'dev_requirement' => false,
+        ),
+    ),
+);
